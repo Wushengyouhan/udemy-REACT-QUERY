7e53251ba3a6299a16165d4f360ba7040d3fdb20
Wushengyouhan <840835067@qq.com>
2025-05-12 11:44:44 +0800

    11. Query Keys

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 export function PostDetail({ post }) {
   // replace with useQuery
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["comments"],
+    queryKey: ["comments", post.id],
     queryFn: () => fetchComments(post.id),
   });
 
