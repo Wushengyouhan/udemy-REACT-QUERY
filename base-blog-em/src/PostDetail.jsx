6a29c223717df5e587c8b9839bbfa2ab1908f5ad
Wushengyouhan <840835067@qq.com>
2025-05-12 14:58:01 +0800

    16. Delete Post with useMutation

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -2,7 +2,7 @@ import { fetchComments } from "./api";
 import "./PostDetail.css";
 import { useQuery } from "@tanstack/react-query";
 
-export function PostDetail({ post }) {
+export function PostDetail({ post, deleteMutation }) {
   // replace with useQuery
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["comments", post.id],
@@ -21,7 +21,10 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>
+        Delete
+      </button>{" "}
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
