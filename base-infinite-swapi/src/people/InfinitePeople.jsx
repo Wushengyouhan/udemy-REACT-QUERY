473b98e64051c64e4879f5ac999e8be24ce194f3
Wushengyouhan <840835067@qq.com>
2025-05-13 11:21:45 +0800

    28. useInfiniteQuery Fetching and Error states

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -4,40 +4,55 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.py4e.com/api/people/";
 const fetchUrl = async (url) => {
+  // await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isLoading,
+    isError,
+    error,
+  } = useInfiniteQuery({
     queryKey: ["sw-people"],
     queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
     getNextPageParam: (lastPage) => {
       return lastPage.next || undefined;
     },
   });
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div className="error">Error! {error.toString()}</div>;
   return (
-    <InfiniteScroll
-      loadMore={() => {
-        if (!isFetching) {
-          fetchNextPage();
-        }
-      }}
-      hasMore={hasNextPage}
-    >
-      {data.pages.map((pageData) => {
-        return pageData.results.map((person) => {
-          return (
-            <Person
-              key={person.name}
-              name={person.name}
-              hairColor={person.hair_color}
-              eyeColor={person.eye_color}
-            />
-          );
-        });
-      })}
-    </InfiniteScroll>
+    <>
+      {isFetching && <div className="loading">Fetching...</div>}
+      <InfiniteScroll
+        loadMore={() => {
+          if (!isFetching) {
+            fetchNextPage();
+          }
+        }}
+        hasMore={hasNextPage}
+        initialLoad={false}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
   );
 }
