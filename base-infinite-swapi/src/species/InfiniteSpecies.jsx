0fbb5fed3267df1fcf929c54f6fb6a2035b300e3
Wushengyouhan <840835067@qq.com>
2025-05-13 11:47:25 +0800

    29. Code Quiz! Infinite Species

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,7 +1,8 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-const initialUrl = "https://swapi.dev/api/species/";
+const initialUrl = "https://swapi.py4e.com/api/species/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
@@ -9,5 +10,47 @@ const fetchUrl = async (url) => {
 
 export function InfiniteSpecies() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isLoading,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ["sw-species"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div className="error">Error! {error.toString()}</div>;
+  return (
+    <>
+      {isFetching && <div className="loading">Fetching...</div>}
+      <InfiniteScroll
+        loadMore={() => {
+          if (!isFetching) fetchNextPage();
+        }}
+        hasMore={hasNextPage}
+        initialLoad={false}
+      >
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            return (
+              <Species
+                key={species.name}
+                name={species.name}
+                language={species.language}
+                averageLifespan={species.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
