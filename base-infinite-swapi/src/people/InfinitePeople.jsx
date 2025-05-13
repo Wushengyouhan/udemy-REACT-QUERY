2e94edbae47043d9b3312b6d02d7ac692235dd7c
Wushengyouhan <840835067@qq.com>
2025-05-13 10:28:11 +0800

    25. Write useInfiniteQuery Call

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,7 +1,8 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = "https://swapi.py4e.com/api/people/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
@@ -9,5 +10,12 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ["sw-people"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
   return <InfiniteScroll />;
 }
