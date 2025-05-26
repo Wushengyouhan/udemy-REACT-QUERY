cd933bd63ef63957777c2efdd5fbda268bd585d5
Wushengyouhan <840835067@qq.com>
2025-05-26 15:35:43 +0800

    73. Writing Optimistic Update

--- base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
+++ base-lazy-days/client/src/components/user/hooks/usePatchUser.ts
@@ -1,11 +1,15 @@
 import jsonpatch from "fast-json-patch";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import type { User } from "@shared/types";
 
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 import { useUser } from "./useUser";
+
 import { toast } from "@/components/app/toast";
+import { queryKeys } from "@/react-query/constants";
+
+export const MUTATION_KEY = "patch-user";
 
 // for when we need a server function
 async function patchUserOnServer(
@@ -29,12 +33,18 @@ async function patchUserOnServer(
 
 export function usePatchUser() {
   const { user, updateUser } = useUser();
+  const queryClient = useQueryClient();
 
   const { mutate: patchUser } = useMutation({
+    mutationKey: [MUTATION_KEY],
     mutationFn: (newData: User) => patchUserOnServer(newData, user),
-    onSuccess: (userData: User | null) => {
+    onSuccess: () => {
       toast({ title: "user updated!", status: "success" });
-      updateUser(userData);
+    },
+    onSettled: () => {
+      return queryClient.invalidateQueries({
+        queryKey: [queryKeys.user],
+      });
     },
   });
 
