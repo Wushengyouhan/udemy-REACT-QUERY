176ab517c559c9d3ec16d426984c8a76214551d8
Wushengyouhan <840835067@qq.com>
2025-05-27 11:30:51 +0800

    79. Code Quiz! Test Rendered Staff Data

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,7 +1,13 @@
 import { AllStaff } from "../AllStaff";
 
-// import { render, screen } from "@/test-utils";
+import { render, screen } from "@/test-utils";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
   // write test here
+  render(<AllStaff />);
+  const staffNames = await screen.findAllByRole("heading", {
+    name: /sandra|divya|mateo|michael/i,
+  });
+
+  expect(staffNames).toHaveLength(4);
 });
